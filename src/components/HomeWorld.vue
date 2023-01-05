302b3359a13f179907c31ad40af8066cd89ed716
Santosh <santosh.kumarnayak.rdp@lseg.com>
2023-01-05 16:33:18 +0530

    Added Home world file

---
+++ src/components/HomeWorld.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>This is our Home</h1>
+    <h2>how are you</h2>
+    <h3> {{data}} </h3>
+    <h4> {{ msg }} </h4>
+    <button v-on:mousehover="hello('Spam')">Call Hello Function</button>
+</template>  
+
+<script>
+
+export default {
+    name: 'HomeWorld',
+    props : {
+        data: String,
+        msg : String 
+    },
+    methods : {
+        hello(data){
+            alert(data);
+        }
+    }
+}
+</script>
